47ea22fa814559fcca3fe40fff64068a2f27c692
Fire <Fire@7acbaa96-8c30-7548-8997-aeb296958e10>
2012-02-09 05:42:45 +0000

    optimized string list format
    git-svn-id: https://eriforce.eicp.net:8443/svn/WhereAreThem@12 7acbaa96-8c30-7548-8997-aeb296958e10

--- Model/StringPersistence.cs
+++ Model/StringPersistence.cs
@@ -7,7 +7,6 @@ using System.Text;
 namespace WhereAreThem.Model {
     public class StringPersistence : IPersistence {
         private const char columnSeparator = '|';
-        private const char folderFileSeparator = '.';
         private const string rowFormat = "{0}{1}{2}";
         private const string folderFormat = "{1}{0}{2}";
         private const string fileFormat = "{1}{0}{2}{0}{3}{0}{4}";
@@ -28,7 +27,6 @@ namespace WhereAreThem.Model {
                 foreach (Folder f in folder.Folders) {
                     Save(f, level, sw);
                 }
-            sw.WriteLine(GetRow(level, folderFileSeparator.ToString()));
             if (folder.Files != null)
                 foreach (File f in folder.Files) {
                     sw.WriteLine(GetRow(level, GetFileString(f)));
@@ -38,7 +36,6 @@ namespace WhereAreThem.Model {
         private string GetRow(int level, string itemString) {
             return string.Format(rowFormat, level, columnSeparator, itemString);
         }
-
 
         private string GetFolderString(Folder folder) {
             return string.Format(folderFormat, columnSeparator,
@@ -54,11 +51,10 @@ namespace WhereAreThem.Model {
             using (StreamReader sr = new FileInfo(path).OpenText()) {
                 string line = sr.ReadLine();
                 string[] parts = line.Split(columnSeparator);
+                int folderLinePartsLength = parts.Length;
                 Dictionary<int, Folder> recentFolders = new Dictionary<int, Folder>() {
                     { 0, GetFolder(parts) }
                 };
-                int prevLevel = 0;
-                bool isFile = false;
 
                 while (!sr.EndOfStream) {
                     line = sr.ReadLine();
@@ -67,41 +63,26 @@ namespace WhereAreThem.Model {
                     parts = line.Split(columnSeparator);
 
                     int currentLevel = GetLevel(parts);
-                    if (currentLevel == prevLevel) {
-                        if (GetName(parts) == folderFileSeparator.ToString())
-                            isFile = true;
-                        else if (!isFile)
-                            AddFolder(parts, recentFolders, currentLevel);
-                        else {
-                            int parent = currentLevel - 1;
-                            if (recentFolders[parent].Files == null)
-                                recentFolders[parent].Files = new List<File>();
-                            recentFolders[parent].Files.Add(GetFile(parts));
-                        }
+                    int parent = currentLevel - 1;
+                    if (parts.Length == folderLinePartsLength) {
+                        Folder f = GetFolder(parts);
+                        if (recentFolders[parent].Folders == null)
+                            recentFolders[parent].Folders = new List<Folder>();
+                        recentFolders[parent].Folders.Add(f);
+
+                        if (recentFolders.ContainsKey(currentLevel))
+                            recentFolders[currentLevel] = f;
+                        else
+                            recentFolders.Add(currentLevel, f);
                     }
                     else {
-                        if (GetName(parts) == folderFileSeparator.ToString())
-                            isFile = true;
-                        else
-                            AddFolder(parts, recentFolders, currentLevel);
+                        if (recentFolders[parent].Files == null)
+                            recentFolders[parent].Files = new List<File>();
+                        recentFolders[parent].Files.Add(GetFile(parts));
                     }
-                    prevLevel = currentLevel;
                 }
                 return recentFolders[0];
             }
-        }
-
-        private void AddFolder(string[] parts, Dictionary<int, Folder> recentFolders, int currentLevel) {
-            int parent = currentLevel - 1;
-            Folder f = GetFolder(parts);
-            if (recentFolders[parent].Folders == null)
-                recentFolders[parent].Folders = new List<Folder>();
-            recentFolders[parent].Folders.Add(f);
-
-            if (recentFolders.ContainsKey(currentLevel))
-                recentFolders[currentLevel] = f;
-            else
-                recentFolders.Add(currentLevel, f);
         }
 
         private Folder GetFolder(string[] lineParts) {
