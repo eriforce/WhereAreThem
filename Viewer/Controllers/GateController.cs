dd4471f0f1a993f39349df28ca2d8f2486080f38
Fire <Fire@7acbaa96-8c30-7548-8997-aeb296958e10>
2012-02-29 08:56:07 +0000

    updated validation
    git-svn-id: https://eriforce.eicp.net:8443/svn/WhereAreThem@47 7acbaa96-8c30-7548-8997-aeb296958e10

---
+++ Viewer/Controllers/GateController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WhereAreThem.Viewer.Models;
+
+namespace WhereAreThem.Viewer.Controllers
+{
+    public class GateController : Controller
+    {
+        public ActionResult Login()
+        {
+            if ((Request.ClientCertificate != null) && Request.ClientCertificate.IsValid
+                    && (Request.ClientCertificate.Issuer == Request.ClientCertificate.ServerIssuer)) {
+                FormsAuthentication.SetAuthCookie(Request.ClientCertificate.Subject.Substring(3), true);
+                return RedirectToAction(Extensions.ActionIndex, Extensions.ControllerHome);
+            }
+            return new HttpStatusCodeResult(401);
+        }
+    }
+}
