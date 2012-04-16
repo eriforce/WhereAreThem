0e67187ea0da31bc433a7e58a1b380f62de154a8
Fire <Fire@7acbaa96-8c30-7548-8997-aeb296958e10>
2012-04-16 13:50:14 +0000

    updated PropertiesWindow
    git-svn-id: https://eriforce.eicp.net:8443/svn/WhereAreThem@87 7acbaa96-8c30-7548-8997-aeb296958e10

--- WinViewer/ViewModel/PropertiesWindowViewModel.cs
+++ WinViewer/ViewModel/PropertiesWindowViewModel.cs
@@ -1,25 +1,60 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
+using PureLib.Common;
 using PureLib.WPF;
 using WhereAreThem.Model;
 
 namespace WhereAreThem.WinViewer {
     public class PropertiesWindowViewModel : ViewModelBase {
-        private List<Folder> _itemStack;
+        private List<Folder> _itemStack {  get; set; }
+        private PropertyInfo _propertyInfo { get; set; }
 
         public FileSystemItem Item { get; private set; }
 
+        public string FileSystemType {
+            get {
+                return Item.GetType().Name;
+            }
+        }
+
         public string Location {
             get {
-                return System.IO.Path.GetFullPath(Item.Name).ToLower();
+                return System.IO.Path.Combine(_itemStack.Select(f => f.Name).ToArray());
             }
+        }
+
+        public string Size {
+            get {
+                return string.Format("{0} ({1} bytes)", _propertyInfo.TotalSizeFriendlyString, _propertyInfo.TotalSizeString);
+            }
+        }
+
+        public string Contains {
+            get {
+                string strFilesCount = string.Empty;
+                if (IsFolder) {
+                    Folder folder = (Folder)Item;
+                    strFilesCount = string.Format("{0} Files, {1} Folders", _propertyInfo.FolderCountString, _propertyInfo.FileCountString);
+                }
+                return strFilesCount;
+            }
+        }
+
+        public bool IsFolder {
+            get { return !IsFile; }
+        }
+
+        public bool IsFile {
+            get { return Item is File; }
         }
 
         public PropertiesWindowViewModel(FileSystemItem item, List<Folder> itemStack) {
             Item = item;
             _itemStack = itemStack;
+            _propertyInfo = new PropertyInfo(_itemStack.Last(), new string[] { Item.Name });
         }
     }
 }
