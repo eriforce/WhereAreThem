4ba234af6816979edff647edc9bf8ad91abeae87
Fire <Fire@7acbaa96-8c30-7548-8997-aeb296958e10>
2012-04-17 12:03:23 +0000

    updated persistence initialization
    git-svn-id: https://eriforce.eicp.net:8443/svn/WhereAreThem@90 7acbaa96-8c30-7548-8997-aeb296958e10

--- Model/Constant.cs
+++ Model/Constant.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Text;
+using PureLib.Common;
 
 namespace WhereAreThem.Model {
     public static class Constant {
         public const string ListExt = "wat";
 
         public static IPersistence GetPersistence() {
-            Type type = Type.GetType(ConfigurationManager.AppSettings["persistence"]);
+            string persistenceType = ConfigurationManager.AppSettings["persistence"];
+            string typeArgs = ConfigurationManager.AppSettings["persistenceArgs"];
+
+            Type type = Type.GetType(persistenceType);
+            if (!typeArgs.IsNullOrEmpty())
+                type = type.MakeGenericType(Type.GetType(typeArgs));
             return Activator.CreateInstance(type) as IPersistence;
         }
     }
