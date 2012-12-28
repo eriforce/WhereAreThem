cb849b2aac8a2dc285d65a6b97e2a42cbac995bb
Fire <Fire@7acbaa96-8c30-7548-8997-aeb296958e10>
2012-12-28 06:24:11 +0000

    updated with PureLib 119
    git-svn-id: https://eriforce.eicp.net:8443/svn/WhereAreThem@163 7acbaa96-8c30-7548-8997-aeb296958e10

--- WebViewer/Controllers/GateController.cs
+++ WebViewer/Controllers/GateController.cs
@@ -7,32 +7,16 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
-using PureLib.Common;
+using PureLib.AspNet;
 using WhereAreThem.WebViewer.Models;
 
 namespace WhereAreThem.WebViewer.Controllers {
     public class GateController : Controller {
         public ActionResult Login() {
-            if (Debugger.IsAttached)
-                FormsAuthentication.SetAuthCookie("Debug", false);
-
-            if (!ValidateClientCertificate(Request.ClientCertificate))
-                return new HttpStatusCodeResult((int)HttpStatusCode.Unauthorized);
+            if (Request.ClientCertificate.Authenticate(ConfigurationManager.AppSettings["issuer"]))
+                return RedirectToAction(Extensions.ActionIndex, Extensions.ControllerHome);
             else
-                FormsAuthentication.SetAuthCookie(Request.ClientCertificate.Subject.Substring("CN=".Length), false);
-
-            return RedirectToAction(Extensions.ActionIndex, Extensions.ControllerHome);
-        }
-
-        private bool ValidateClientCertificate(HttpClientCertificate cert) {
-            if (cert == null || !cert.IsValid)
-                return false;
-
-            string validIssuer = ConfigurationManager.AppSettings["issuer"];
-            if (!validIssuer.IsNullOrEmpty() && !cert.BinaryIssuer.ToHexString().Equals(validIssuer, StringComparison.OrdinalIgnoreCase))
-                return false;
-
-            return true;
+                return new HttpStatusCodeResult((int)HttpStatusCode.Unauthorized);
         }
     }
 }
