dc98bd8bc979174b5fcf1da9b62859fc30a4fa77
Fire <Fire@7acbaa96-8c30-7548-8997-aeb296958e10>
2013-09-13 08:20:04 +0000

    fixed charset issue on P/Invokes
    git-svn-id: https://eriforce.eicp.net:8443/svn/WhereAreThem@184 7acbaa96-8c30-7548-8997-aeb296958e10

--- WinViewer/NativeMethods.cs
+++ WinViewer/NativeMethods.cs
@@ -11,15 +11,15 @@ namespace WhereAreThem.WinViewer {
         [DllImport("gdi32.dll", SetLastError = true)]
         public static extern bool DeleteObject(IntPtr hObject);
 
-        [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
+        [DllImport("shell32.dll", CharSet = CharSet.Ansi)]
         public static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref Shell32.SHFILEINFO psfi, uint cbFileInfo, uint uFlags);
 
-        [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
+        [DllImport("shell32.dll", CharSet = CharSet.Ansi)]
         public static extern IntPtr ExtractIcon(IntPtr hInst, string lpszExeFileName, int nIconIndex);
 
-        [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
+        [DllImport("shell32.dll", CharSet = CharSet.Ansi)]
         public static extern int ExtractIconEx(string stExeFileName, int nIconIndex, ref IntPtr phiconLarge, ref IntPtr phiconSmall, int nIcons);
-    
+
         /// <summary>
         /// Wraps necessary functions imported from User32.dll. Code courtesy of MSDN Cold Rooster Consulting example.
         /// Provides access to function required to delete handle. This method is used internally
