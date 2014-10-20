5dadf5bf0f97520736d7426b565b2597d6486f77
Erich <Erich@7acbaa96-8c30-7548-8997-aeb296958e10>
2014-10-20 03:10:56 +0000

    added bundling
    git-svn-id: https://eriforce.eicp.net:8443/svn/WhereAreThem@207 7acbaa96-8c30-7548-8997-aeb296958e10

--- WebViewer/Global.asax.cs
+++ WebViewer/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace WhereAreThem.WebViewer {
@@ -10,6 +11,14 @@ namespace WhereAreThem.WebViewer {
     // visit http://go.microsoft.com/?LinkId=9394801
 
     public class MvcApplication : System.Web.HttpApplication {
+        public static void RegisterBundles(BundleCollection bundles) {
+            bundles.Add(new ScriptBundle("~/js/site").Include(
+                "~/Scripts/jquery.explorer.js"));
+
+            bundles.Add(new StyleBundle("~/css/site").Include(
+                "~/Content/Site.css"));
+        }
+
         public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
             filters.Add(new HandleErrorAttribute());
         }
@@ -28,6 +37,7 @@ namespace WhereAreThem.WebViewer {
         protected void Application_Start() {
             AreaRegistration.RegisterAllAreas();
 
+            RegisterBundles(BundleTable.Bundles);
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
         }
