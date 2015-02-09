47ba1fa22e68fc70fba03a58899cf57f86ca0104
Erich <eriforce@live.com>
2015-02-09 11:28:01 +0800

    referenced MediaInfoWrapper

--- Model/Plugins/MediaInfoPlugin.cs
+++ Model/Plugins/MediaInfoPlugin.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MediaInfoWrapper;
 using PureLib.Common;
 
 namespace WhereAreThem.Model.Plugins {
