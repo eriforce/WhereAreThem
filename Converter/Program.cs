e69b523c6e14813cde96df45088df022e16c4162
Erich <eriforce@live.com>
2016-03-25 13:11:21 +0800

    updated references of purelib

--- Converter/Program.cs
+++ Converter/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Text;
-using PureLib.Common;
 using WhereAreThem.Model;
 using WhereAreThem.Model.Models;
 using WhereAreThem.Model.Persistences;
@@ -15,7 +14,7 @@ namespace WhereAreThem.Converter {
             IPersistence source = new TextPersistence();
             IPersistence target = new CompressedPersistence<TextPersistence>();
 
-            string[] lists = IO.Directory.GetFiles(ConfigurationManager.AppSettings["outputPath"], 
+            string[] lists = IO.Directory.GetFiles(ConfigurationManager.AppSettings["outputPath"],
                 "*.*.wat", IO.SearchOption.AllDirectories);
             foreach (string file in lists) {
                 Folder folder = source.Load(file);
