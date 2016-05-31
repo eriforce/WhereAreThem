d6fc81e8ea5d9b652a8328ef435ac02f07139b87
Erich <eriforce@live.com>
2016-05-31 22:07:02 +0800

    fixed login error when issuer not set

--- WebViewer/Models/ClientCertificateExtensions.cs
+++ WebViewer/Models/ClientCertificateExtensions.cs
@@ -10,27 +10,26 @@ using PureLib.Common;
 
 namespace WhereAreThem.WebViewer.Models {
     public static class ClientCertificateExtensions {
-        public static bool CertAuth(this HttpRequestBase request, string issuer = null) {
-            if (Debugger.IsAttached)
-                FormsAuthentication.SetAuthCookie("Debug", false, request.ApplicationPath);
-
-            HttpClientCertificate cert = request.ClientCertificate;
-            if (ValidateClientCertificate(cert, issuer))
-                FormsAuthentication.SetAuthCookie(cert.Subject.Substring("CN=".Length), false, request.ApplicationPath);
-            else
+        public static bool CertAuth(this HttpRequestBase request, string issuer) {
+            string userName = ParseUserName(request.ClientCertificate, issuer);
+            if (userName.IsNullOrEmpty())
                 return false;
 
+            FormsAuthentication.SetAuthCookie(userName, false, request.ApplicationPath);
             return true;
         }
 
-        private static bool ValidateClientCertificate(HttpClientCertificate cert, string issuer) {
-            if ((cert == null) || !cert.IsValid)
-                return false;
+        private static string ParseUserName(HttpClientCertificate cert, string issuer) {
+            if (Debugger.IsAttached)
+                return "Debug";
 
-            if (!issuer.IsNullOrEmpty() && !cert.BinaryIssuer.ToHexString().Equals(issuer, StringComparison.OrdinalIgnoreCase))
-                return false;
+            if (issuer.IsNullOrEmpty())
+                return "Guest";
 
-            return true;
+            if (cert != null && cert.IsValid && cert.BinaryIssuer.ToHexString().Equals(issuer, StringComparison.OrdinalIgnoreCase))
+                return cert.Subject.Substring("CN=".Length);
+
+            return null;
         }
     }
 }
