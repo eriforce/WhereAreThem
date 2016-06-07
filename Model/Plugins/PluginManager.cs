58ede1c6a729f7490e4704dbd7971c0ea81e2304
Claus Zhong <zhongyufei@dtcj.com>
2016-06-07 22:58:51 +0800

    updated plugin manager with mef

--- Model/Plugins/PluginManager.cs
+++ Model/Plugins/PluginManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -8,27 +10,41 @@ using WhereAreThem.Model.Models;
 
 namespace WhereAreThem.Model.Plugins {
     public class PluginManager {
-        private Dictionary<string, IPlugin> _plugins = new Dictionary<string, IPlugin>(StringComparer.OrdinalIgnoreCase);
+        [ImportMany]
+        IEnumerable<Lazy<IPlugin, IPluginMetaData>> _imported_plugins;
+
+        private Dictionary<string, List<Lazy<IPlugin, IPluginMetaData>>> _plugins = new Dictionary<string, List<Lazy<IPlugin, IPluginMetaData>>>(StringComparer.OrdinalIgnoreCase);
 
         public PluginManager() {
-            Type pluginType = typeof(IPlugin);
-            foreach (IPlugin plugin in pluginType.Assembly.GetTypes()
-                    .Where(t => pluginType.IsAssignableFrom(t) && t.IsClass)
-                    .Select(t => Activator.CreateInstance(t))) {
-                if (plugin.Loaded && (plugin.Extensions != null))
-                    foreach (string ext in plugin.Extensions) {
-                        _plugins.Add(ext, plugin);
-                    }
+            CompositionContainer container = new CompositionContainer(new DirectoryCatalog("."));
+            container.ComposeParts(this);
+
+            foreach (var p in _imported_plugins) {
+                foreach (string ext in p.Metadata.Extensions) {
+                    if (_plugins.ContainsKey(ext))
+                        _plugins[ext].Add(p);
+                    else
+                        _plugins.Add(ext, new List<Lazy<IPlugin, IPluginMetaData>>() { p });
+                }
             }
         }
 
         public Dictionary<string, string> GetDescriptions(string path, bool hasChanged, Dictionary<string, string> descriptions) {
             string ext = Path.GetExtension(path);
+
             if (!_plugins.ContainsKey(ext))
                 return null;
 
-            // TODO: implement plugin invocations
-            throw new NotImplementedException();
+            Dictionary<string, string> new_descriptions = new Dictionary<string, string>();
+
+            foreach (var p in _plugins[ext]) {
+                if (hasChanged || !descriptions.ContainsKey(p.Metadata.ID))
+                    new_descriptions.Add(p.Metadata.ID, p.Value.GetDescription(path));
+                else
+                    new_descriptions.Add(p.Metadata.ID, descriptions[p.Metadata.ID]);
+            }
+
+            return new_descriptions;
         }
     }
 }
