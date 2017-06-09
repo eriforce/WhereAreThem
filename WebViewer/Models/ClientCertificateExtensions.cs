3524dcb78620e6c928f91618e0568ef1d8b19382
Erich <eriforce@live.com>
2017-06-09 14:16:03 +0800

    updated authentication

--- WebViewer/Models/ClientCertificateExtensions.cs
+++ WebViewer/Models/ClientCertificateExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -11,26 +12,22 @@ using PureLib.Common;
 namespace WhereAreThem.WebViewer.Models {
     public static class ClientCertificateExtensions {
         public static bool CertAuth(this HttpRequestBase request, string issuer) {
-            string userName = ParseUserName(request.ClientCertificate, issuer);
-            if (userName.IsNullOrEmpty())
+            HttpClientCertificate cert = request.ClientCertificate;
+            if (cert == null || !cert.IsValid)
                 return false;
 
+            X509Certificate2 x509 = new X509Certificate2(cert.Certificate);
+            X509Chain chain = new X509Chain(true);
+            chain.ChainPolicy.RevocationMode = X509RevocationMode.Offline;
+            chain.Build(x509);
+
+            if (!chain.ChainElements.Cast<X509ChainElement>().Any(e =>
+                    e.Certificate.Thumbprint.Equals(issuer, StringComparison.OrdinalIgnoreCase)))
+                return false;
+
+            string userName = x509.GetNameInfo(X509NameType.SimpleName, false);
             FormsAuthentication.SetAuthCookie(userName, false, request.ApplicationPath);
             return true;
-        }
-
-        private static string ParseUserName(HttpClientCertificate cert, string issuer) {
-            if (issuer.IsNullOrEmpty())
-                throw new ApplicationException("Issuer has not been set.");
-
-            if (Debugger.IsAttached)
-                return "Debug";
-
-            if (cert != null && cert.IsValid && !cert.Subject.IsNullOrEmpty() &&
-                    cert.BinaryIssuer.ToHexString().Equals(issuer, StringComparison.OrdinalIgnoreCase))
-                return cert.Subject.Substring("CN=".Length);
-
-            return null;
         }
     }
 }
